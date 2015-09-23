6a6e6c10cd5b78116ac80cc7a2651c96baf78db2
Iago Silva <iagorrs@gmail.com>
2015-09-23 11:12:25 -0300

    23-09.1

--- JavaTCPINTERFACEGRAFICA/src/Control/ClienteRecebeMensagem.java
+++ JavaTCPINTERFACEGRAFICA/src/Control/ClienteRecebeMensagem.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JTextArea;
 
 /**
@@ -15,39 +16,48 @@ public class ClienteRecebeMensagem extends Thread {
     Socket socket;
     JTextArea areaTexto;
     ArrayList<PrintStream> streamsSaida;
-    
-    public ClienteRecebeMensagem (Socket socket, JTextArea areaTexto){
+
+    public ClienteRecebeMensagem(Socket socket, JTextArea areaTexto) {
         this.socket = socket;
         this.areaTexto = areaTexto;
     }
-    
+
     @Override
-    public synchronized void run(){
-/*        try {
+    public synchronized void run() {
+        try {
             Scanner s = new Scanner(socket.getInputStream());
             while (s.hasNextLine()) {
+                String encriptado = s.nextLine();
+                System.out.println(encriptado);
+                AES seguranca = new AES(null);
+                String resultado = seguranca.decrypt(encriptado.getBytes());
                 areaTexto.setText(areaTexto.getText() + "\n" + s.nextLine());
             }
-            
-        } catch (IOException ex) {
-            Logger.getLogger(ClienteRecebeMensagem.class.getName()).log(Level.SEVERE, null, ex);
-        }
-  */
-        try{
-            InputStream is = socket.getInputStream();
-            AES seguranca = new AES(null);
-            
-            while(true){
-                int dado;
-                byte[] recebimento = new byte[1024];
-                is.read(recebimento);
-                String resultado = seguranca.decrypt(recebimento);
-                areaTexto.setText(areaTexto.getText() + "\n" + resultado);
-            }
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-    }  
+        /*
+         try{
+         InputStream is = socket.getInputStream();
+         AES seguranca = new AES(null);
+            
+         while(true){
+         int dado;
+         String encriptado = "";
+                
+         while((dado=is.read())!=-1){
+         encriptado = encriptado + (char)dado;
+         }
+                
+         String resultado = seguranca.decrypt(encriptado.getBytes());
+                
+         areaTexto.setText(areaTexto.getText() + "\n" + resultado);
+         }
+            
+         }catch(Exception e){
+         e.printStackTrace();
+         }
+         */
+    }
 }
